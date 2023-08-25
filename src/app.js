12e01927ce0b6e7ea58f8574ca3571fefe745ec6
jeanpierre <alejandrovelaarana@gmail.com>
2023-08-25 18:24:26 -0300

    url nueva

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin : 'https://poem-xi.vercel.app/',
+    origin : 'https://poem-7h96kcvr7-jeanvela.vercel.app/',
     // origin: '*',
     credentials: true
 }))
